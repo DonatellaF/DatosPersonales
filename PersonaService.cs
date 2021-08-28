0434e88432a3de0f8d8002a1633642369c75d92b
Emiliano Neira <Emilianoneira_@hotmail.com>
2021-08-28 13:02:36 -0300

    first commit

---
+++ PersonaService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatosPersonalesApp
+{
+    public class PersonaService
+    {/// <summary>
+    /// Devuelve una persona con nombre y apellido.
+    /// </summary>
+    /// <returns></returns>
+        public Persona GetDatosPersonales()
+        {
+            //TODO apellido nulo, nombre nulo.
+            //TODO solo letras.
+            var persona = new Persona();
+            Console.WriteLine("Ingrese los siguientes datos personales:");
+            Console.WriteLine("Nombre:");
+            persona.Nombre = Console.ReadLine();
+            Console.WriteLine("Apellido:");
+            persona.Apellido = Console.ReadLine();
+            return persona;
+        }
+        /// <summary>
+        /// solicita por consola una fecha, si es valida la devuelve y sino lanza una excepcion.
+        /// </summary>
+        /// <returns></returns>
+        public DateTime GetFechaNacimiento()
+        {
+            //TODO controlar que sea mayor de edad.
+            string fecha;
+            DateTime fechaNacimiento;
+            do
+            {
+                Console.WriteLine("Ingrese su fecha de nacimiento: dd/mm/aaaa");
+                fecha = Console.ReadLine();
+            }
+            while (!DateTime.TryParse(fecha, out fechaNacimiento) && Continuar());
+            return fechaNacimiento;
+        }
+        /// <summary>
+        /// Si la respuesta es s o S, devuelve true.
+        /// </summary>
+        /// <returns></returns>
+        private bool Continuar()
+        {
+            Console.WriteLine("¿Desea volver a intentarlo? Presione s si desea volver a intentarlo, de lo contrario presione n");
+            string respuesta = Console.ReadLine().ToUpper();
+
+            return respuesta == "S";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatosPersonalesApp
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var personaService = new PersonaService();
+
+            
+            Persona persona = personaService.GetDatosPersonales();
+            Console.WriteLine($"Bienvenido {persona.Nombre}");
+
+            persona.FechaNaciciento = personaService.GetFechaNacimiento();
+            Console.ReadLine();
+        }
+    }
+}
